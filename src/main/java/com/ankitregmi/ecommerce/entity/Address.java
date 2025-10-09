18095579c4e8a134f959ff0b2b7a56f28f229e48
Ankit Regmi <regmiankit324@gmial.com>
2025-10-09 09:42:47 +0900

    Simplified checkout and address structure

--- src/main/java/com/ankitregmi/ecommerce/entity/Address.java
+++ src/main/java/com/ankitregmi/ecommerce/entity/Address.java
@@ -1,3 +1,41 @@
+//package com.ankitregmi.ecommerce.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Table(name = "address")
+//@Getter
+//@Setter
+//
+//public class Address {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+//    private long id;
+//
+//    @Column(name = "street")
+//    private String street;
+//
+//    @Column(name = "city")
+//    private String city;
+//
+//    @Column(name = "state")
+//    private String state;
+//
+//    @Column(name = "country")
+//    private String country;
+//
+//    @Column(name = "zip_code")
+//    private String zipCode;
+//
+//    @OneToOne
+//    @PrimaryKeyJoinColumn
+//    private Order order;
+//}
+
 package com.ankitregmi.ecommerce.entity;
 
 import jakarta.persistence.*;
@@ -8,7 +46,6 @@ import lombok.Setter;
 @Table(name = "address")
 @Getter
 @Setter
-
 public class Address {
 
     @Id
@@ -16,20 +53,23 @@ public class Address {
     @Column(name = "id")
     private long id;
 
-    @Column(name = "street")
-    private String street;
-
-    @Column(name = "city")
-    private String city;
-
-    @Column(name = "state")
-    private String state;
-
     @Column(name = "country")
     private String country;
 
-    @Column(name = "zip_code")
-    private String zipCode;
+    // Optional: If you want to keep compatibility with existing DB records
+    // you can safely comment out or delete the unused fields below:
+    //
+    // @Column(name = "street")
+    // private String street;
+    //
+    // @Column(name = "city")
+    // private String city;
+    //
+    // @Column(name = "state")
+    // private String state;
+    //
+    // @Column(name = "zip_code")
+    // private String zipCode;
 
     @OneToOne
     @PrimaryKeyJoinColumn
